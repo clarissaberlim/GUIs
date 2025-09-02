a7fe18ee064c845387670df2be68d1823e344dc9
Clarissa <clarissaberlim@gmail.com>
2025-09-02 09:53:30 -0300

    Update main.c

--- ex1.5.1/main.c
+++ ex1.5.1/main.c
@@ -1,10 +1,9 @@
 #include <SDL2/SDL.h>
 
-int main (int argc, char* args[])
-{
+int main (int argc, char* args[]) {
     /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* win = SDL_CreateWindow("Movendo um Retângulo",
+    SDL_Window* win = SDL_CreateWindow("Retângulos",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
                          400, 200, SDL_WINDOW_SHOWN
@@ -13,14 +12,14 @@ int main (int argc, char* args[])
 
     /* EXECUÇÃO */
     SDL_Rect r = { 40,20, 10,10 };
-    SDL_Rect r2 = { 40,20, 20,30 };
-    SDL_Rect r3 = { 40,20, 50,60 };
+    SDL_Rect r2 = { 60,50, 10,20 };
+    SDL_Rect r3 = { 20,80, 20,10 };
     
     SDL_Event evt;
     int espera = 500;
     
     while (1) {
-        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00); //fundo branco
         SDL_RenderClear(ren);
 
         SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
@@ -33,21 +32,23 @@ int main (int argc, char* args[])
         SDL_RenderFillRect(ren, &r3);
 
         SDL_RenderPresent(ren);
+        
         SDL_WaitEvent(&evt);
         Uint32 antes = SDL_GetTicks();
         int isevt = SDL_WaitEventTimeout(&evt, espera);
+        
         if(isevt){
-            espera -=(SDL_GetTicks()-antes);
+            espera -= (SDL_GetTicks() - antes);
             if (evt.type == SDL_QUIT) {
             	break;
-           }
+			}
             if (evt.type == SDL_KEYDOWN) {
                 switch (evt.key.keysym.sym) {
                     case SDLK_UP:
-                    if (r.y - 5 >= 0) {
-                	    r.y -= 5;
-                    }
-                    break;
+                    	if (r.y - 5 >= 0) {
+                	    	r.y -= 5;
+                    	}
+                    	break;
                     case SDLK_DOWN:
                 	    if (r.y + r.h + 5 <= 100) {
                 		    r.y += 5;
@@ -71,16 +72,13 @@ int main (int argc, char* args[])
                 SDL_GetMouseState(&mX, &mY);
                 r2.x = mX;
                 r2.y = mY;
-
-
             }
+        } else {
+        	espera = 500;
+        	r3.x +=2;
+        	r3.y +=2;
         }
-    else{
-        espera = 500;
-        r3.x +=2;
-        r3.y +=2;
-    }
-}       
+	}       
 
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
