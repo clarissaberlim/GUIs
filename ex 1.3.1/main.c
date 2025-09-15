bb9b07c0ffeb9cdf9725506e9f3e64284b644bd8
Clarissa <clarissaberlim@gmail.com>
2025-09-15 15:18:59 -0300

    Update main.c

--- ex 1.3.1/main.c
+++ ex 1.3.1/main.c
@@ -1,58 +1,62 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-int main (int argc, char* args[]) {
-  /* INICIALIZACAO */
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Window* win = SDL_CreateWindow("Exercício 3",
-  SDL_WINDOWPOS_UNDEFINED,
-  SDL_WINDOWPOS_UNDEFINED,
-  250, 150, SDL_WINDOW_SHOWN
-  );
-  SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
+int main(int argc, char* args[])
+{
+    /* INICIALIZACAO */
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* win = SDL_CreateWindow("Animacao em Ciclo",
+                                       SDL_WINDOWPOS_UNDEFINED,
+                                       SDL_WINDOWPOS_UNDEFINED,
+                                       200, 100, SDL_WINDOW_SHOWN
+                                      );
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
-  /* EXECUÇÃO */
-  SDL_Rect r = {0,0,10,10}; //retangulo 10x10
-  float centro_x = 125, centro_y = 75;  //centro da tela
-  float raio = 50;  //raio do círculo
-  float angulo = 0.0;  //inicializa o ângulo
- 
-  float velocidade = 10.0; //velocidade linear (px/s)
-  float omega = velocidade / raio; //velocidade angular (rad/s)
+    /* EXECUÇÃO */
+    SDL_Rect r = { 0, 45, 10, 10 }; 		// Inicia o retângulo na borda esquerda
+    float posX = r.x; 				// Posição do retângulo como float para maior precisão
+    int direcao = 1; 				// 1 para direita, -1 para esquerda
+    float velocidade = 10.0f; 			// 10 pixels por segundo
 
-  Uint32 ultimo_tempo = SDL_GetTicks();
-  Uint32 inicio = SDL_GetTicks();  //guarda o tempo inicial
-
-  while (1) {
-    Uint32 tempo_atual = SDL_GetTicks();
-    float delta_t = (tempo_atual - ultimo_tempo) / 1000.0f; //em segundos
-    ultimo_tempo = tempo_atual;
-
-    if ((tempo_atual - inicio) >= 10000) { //limita o tempo da animação em 10s
-        break;
-      }
-
-    angulo += omega * delta_t;  //atualiza de acordo com tempo real
-    if (angulo >= 2 * M_PI) {
-        angulo -= 2 * M_PI;
-      }
- 
-    SDL_SetRenderDrawColor(ren,0,0,0,255); //cor de fundo preto
-    SDL_RenderClear(ren);
+    Uint32 tempo_anterior = SDL_GetTicks();
     
-    r.x = centro_x + raio * cos(angulo) - r.w / 2;
-    r.y = centro_y + raio * sin(angulo) - r.h / 2;
+    SDL_Event evt;
+    int rodando = 1; 				// Flag para controlar o loop principal
 
-    SDL_SetRenderDrawColor(ren, 255, 203, 219, 255);
-    SDL_RenderFillRect(ren, &r);
-    SDL_RenderPresent(ren);
-  
-    SDL_Delay(16); //~60FPS
+    while (rodando) {
+        if (SDL_WaitEventTimeout(&evt, 16)) { 	// Espera por até 16ms
+            if (evt.type == SDL_QUIT) {
+                rodando = 0;
+            }
+        }        
+        // Calcula o tempo decorrido desde o último frame
+        Uint32 tempo_atual = SDL_GetTicks();
+        float delta_time = (tempo_atual - tempo_anterior) / 1000.0f;
+        tempo_anterior = tempo_atual;
+
+        posX += direcao * velocidade * delta_time;
+
+        // Mudança de direção
+        if (direcao == 1 && posX + r.w >= 200) {
+            direcao = -1;
+            posX = 200 - r.w; 			// Ajusta a posição para não ultrapassar a borda
+        } else if (direcao == -1 && posX <= 0) {
+            direcao = 1;
+            posX = 0; 				// Ajusta a posição para não ultrapassar a borda
+        }
+
+        r.x = (int)posX; 			// Converte para int para o SDL_Rect
+
+        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_RenderFillRect(ren, &r);
+        SDL_RenderPresent(ren);
     }
-  /* FINALIZACAO */
-  SDL_DestroyRenderer(ren);
-  SDL_DestroyWindow(win);
-  SDL_Quit();
+
+    /* FINALIZACAO */
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    
+    return 0;
 }
