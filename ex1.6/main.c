815a3f6ca32c1c6414f48bc5e562346fbdae9da2
Clarissa <clarissaberlim@gmail.com>
2025-09-16 10:30:40 -0300

    Update main.c

--- ex1.6/main.c
+++ ex1.6/main.c
@@ -1,11 +1,10 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h> 
 
-#define FINISH_LINE   500
-#define SPEED1 3
-#define SPEED2 2
-#define SPEED3 4
+#define CHEGADA   500
 
 int main(int argc, char* args[]) {
     /* INICIALIZAÇÃO */
@@ -18,7 +17,6 @@ int main(int argc, char* args[]) {
 
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-    // Retângulos (corredores)
     SDL_Rect r1 = { 20, 40, 20, 20 };
     SDL_Rect r2 = { 20, 90, 20, 20 };
     SDL_Rect r3 = { 20, 140, 20, 20 };
@@ -26,15 +24,20 @@ int main(int argc, char* args[]) {
     bool quit = false;
     SDL_Event e;
 
+    srand(time(NULL));
+
     while (!quit) {
-        // Reset corrida
         r1.x = r2.x = r3.x = 20;
         bool chegou1 = false, chegou2 = false, chegou3 = false;
         bool vencedorAnunciado = false;
         int vencedor = 0;
 
+        // Velocidades aleatórias entre 1 e 5
+        int speed1 = rand() % 5 + 1;
+        int speed2 = rand() % 5 + 1;
+        int speed3 = rand() % 5 + 1;
+
         while (!(chegou1 && chegou2 && chegou3) && !quit) {
-            // Espera evento OU timeout (30 ms)
             int hasEvent = SDL_WaitEventTimeout(&e, 30);
 
             if (hasEvent) {
@@ -42,36 +45,31 @@ int main(int argc, char* args[]) {
                     quit = true;
                 }
             } else {
-                // Timeout → mover os objetos
-                if (!chegou1) r1.x += SPEED1;
-                if (!chegou2) r2.x += SPEED2;
-                if (!chegou3) r3.x += SPEED3;
+                if (!chegou1) r1.x += speed1;
+                if (!chegou2) r2.x += speed2;
+                if (!chegou3) r3.x += speed3;
             }
 
-            // Checar chegada
-            if (r1.x + r1.w >= FINISH_LINE) {
+            if (r1.x + r1.w >= CHEGADA) {
                 chegou1 = true;
                 if (!vencedorAnunciado) { vencedor = 1; vencedorAnunciado = true; }
             }
-            if (r2.x + r2.w >= FINISH_LINE) {
+            if (r2.x + r2.w >= CHEGADA) {
                 chegou2 = true;
                 if (!vencedorAnunciado) { vencedor = 2; vencedorAnunciado = true; }
             }
-            if (r3.x + r3.w >= FINISH_LINE) {
+            if (r3.x + r3.w >= CHEGADA) {
                 chegou3 = true;
                 if (!vencedorAnunciado) { vencedor = 3; vencedorAnunciado = true; }
             }
 
-            // Renderização
             SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
             SDL_RenderClear(ren);
 
-            // Linha de chegada
             SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
-            SDL_Rect linha = { FINISH_LINE, 0, 5, SCREEN_HEIGHT };
+            SDL_Rect linha = { CHEGADA, 0, 5, 200 };
             SDL_RenderFillRect(ren, &linha);
 
-            // Corredores
             SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
             SDL_RenderFillRect(ren, &r1);
 
@@ -86,11 +84,9 @@ int main(int argc, char* args[]) {
 
         if (!quit) {
             printf("O vencedor foi o objeto %d!\n", vencedor);
-            SDL_Delay(2000); // esperar um pouco antes de reiniciar
         }
     }
 
-    /* FINALIZAÇÃO */
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
